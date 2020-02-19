24536902d449b44bf53a6f33ca2b35699dfc8721
BuildTools <unconfigured@null.spigotmc.org>
2020-02-19 22:03:43 +0800

    Allow game to randomly set a unlocking angle once it starts

---
+++ The lock picking game/Assets/Scripts/SetWantedAngle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetWantedAngle : MonoBehaviour
+{
+    GameObject[] AngleObjectNames;
+    string WantedAngleObjectName = "WantedAngle";
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AngleObjectNames = GameObject.FindGameObjectsWithTag("Angle");
+        index = Random.Range(0, AngleObjectNames.Length);
+        Debug.LogError(index);
+        AngleObjectNames[index].name = WantedAngleObjectName;
+    }
+
+}
